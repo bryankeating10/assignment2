61bb8178d73ab7b00214fce7ed16a0f51ccdad24
Bryan <ru.bryankeating@gmail.com>
2025-09-17 23:15:03 -0400

    Added input hints

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,13 +1,13 @@
-def addition(a, b):
+def addition(a:float, b:float) -> float:
 	return a + b
 
-def subtraction(a, b):
+def subtraction(a:float, b:float) -> float:
 	return a - b
 
-def multiplication(a, b):
+def multiplication(a:float, b:float) -> float:
 	return a * b
 
-def division(a, b):
+def division(a:float, b:float) -> float | str:
 	if b != 0:
 		return a / b
 	else:
